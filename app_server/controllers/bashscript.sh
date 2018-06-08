5fe16a2969a27dda11c5acc274d50031f67b94a8
reynojon <reynojon@oregonstate.edu>
2018-06-07 21:00:23 -0400

    Update bashscript.sh

--- app_server/controllers/bashscript.sh
+++ app_server/controllers/bashscript.sh
@@ -2,30 +2,6 @@
 # bashscript.sh awardType recipientName signatureURL presenterName awardDate recipientEmail
  echo "launching award creation script"
 
-# bashscript.sh awardType recipientName signatureURL presenterName awardDate recipientEmail
- echo "launching award creation script"
-# echo $1 #awardType    
-# echo $2 #recipientName
-echo $3 #signatureURL
-# echo $4 #presenterName
-# echo $5 #awardDate
-# echo $6 #recipientEmail
-node ./app_server/controllers/imageconverter.js $3
-node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.png" "$4" "$5"
-#echo "current directory"
-#ls
-#echo "./app_server/controllers"
-#ls ./app_server/controllers
-#echo "root directory"
-#ls ../../
-#cat ./app_server/controllers/award.tex
-pdflatex ./app_server/controllers/award.tex
-#node ./app_server/controllers/converttex.js
-#ls
-node ./app_server/controllers/mailer.js "$6"
-echo "award creation script done"
-
-
 #create image file from database
 node ./app_server/controllers/imageconverter.js $3
 
@@ -57,7 +33,7 @@ else
 fi
 
 #convert latex file into a pdf
-#node ./app_server/controllers/converttex.js
+pdflatex ./app_server/controllers/award.tex
 
 #send pdf to recipientEmail
 #node ./app_server/controllers/mailer.js "$6"
